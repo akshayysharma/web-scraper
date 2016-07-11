3518a47867f013d95ae3e00f2d5d35b3414f3d1a
Akshay sharma <akshay.sharma76757@gmail.com>
2016-07-11 23:17:05 +0530

    Add files via upload

---
+++ WorkerClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace scrape
+{
+    class WorkerClass
+    {
+        public static string getSoursecode(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            string soursecode = sr.ReadToEnd();
+            sr.Close();
+            resp.Close();
+            return soursecode;
+        }
+
+    }
+}
