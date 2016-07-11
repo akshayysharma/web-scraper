3ff593dfe16b111797446337d6e187167d3db7bc
Akshay sharma <akshay.sharma76757@gmail.com>
2016-07-11 23:22:29 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace scrape
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text; ///convert url into variables
+            string soursecode = WorkerClass.getSoursecode(url);
+            StreamWriter sw = new StreamWriter("website.html");
+            sw.Write(soursecode);
+            sw.Close();
+
+        }
+    }
+}
